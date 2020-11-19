20c2fecc2c08a726bd0a2503d14e20049c89b079
Crowdbotics <team@crowdbotics.com>
2020-11-19 01:41:08 +0000

    remove package blueprint-react-native-sign-up-1-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,6 @@ import SideMenu from './sideMenu';
 import SignIn45175510Navigator from '../features/SignIn45175510/navigator';
 import CalendarView5175509Navigator from '../features/CalendarView5175509/navigator';
 import Dashboard27175507Navigator from '../features/Dashboard27175507/navigator';
-import SignUp14175504Navigator from '../features/SignUp14175504/navigator';
 
 /**
  * new navigators can be imported here
@@ -20,7 +19,6 @@ const AppNavigator = {
 SignIn45175510: { screen: SignIn45175510Navigator },
 CalendarView5175509: { screen: CalendarView5175509Navigator },
 Dashboard27175507: { screen: Dashboard27175507Navigator },
-SignUp14175504: { screen: SignUp14175504Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -9,7 +9,6 @@ import { combineReducers } from "redux";
 import SignIn45175510Reducer from '../features/SignIn45175510/redux/reducers'
 import CalendarView5175509Reducer from '../features/CalendarView5175509/redux/reducers';
 import Dashboard27175507Reducer from '../features/Dashboard27175507/redux/reducers'
-import SignUp14175504Reducer from '../features/SignUp14175504/redux/reducers'
 
 export const combinedReducers = combineReducers({
   blank: (state, action) => {
@@ -22,6 +21,5 @@ export const combinedReducers = combineReducers({
 SignIn45175510: SignIn45175510Reducer,
 CalendarView5175509: CalendarView5175509Reducer,
 Dashboard27175507: Dashboard27175507Reducer,
-SignUp14175504: SignUp14175504Reducer,
 
 });
